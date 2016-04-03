e521ddc46a1d270bc5256b869e93b5d5db0c69f3
justincoh <justincoh@gmail.com>
2016-04-03 15:42:42 -0400

    creates and appends svg to dom, no viz yet

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,53 @@
 "use strict";
-console.log('Script loaded');
 
 let container = d3.select('#container')
 
 let data = null;
-let svg ={
-    width:500,
-    height: 500
-};
+let svgWidth = window.innerWidth * .8,
+    svgheight = 500;
 
+// import data
 d3.csv("./data/rocketLeagueStats_cleaned.csv",(d) => {
-        console.log("loaded");
-        console.log(d);
         data = d;
+        let headers = data.shift();
+
+        //make type adjustments
+        data.forEach(e=>{
+            for(let prop in e){
+                if(prop !== 'Vehicle'){
+                        e[prop] = parseInt(e[prop])
+                }
+            }
+        });
+
+        buildGraph(data,'Rankings');
 });
 
-let xscale = d3.scale.ordinal(),
-    yscale = d3.scale.linear();
 
-xscale.range([0,svg.width]); //tbd
-yscale.range([0,svg.height]);
+// horizontal bar chart
+// make it relative off screen size for mobile
+let buildGraph = function(data, prop){
 
+    let svg = d3.select('#container').append('svg')
+            .attr('width',svgWidth)
+            .attr('height',svgHeight),
+        xScale = d3.scale.linear(),
+        yScale = d3.scale.ordinal(),
+        xDomain = d3.extent(data => data[prop]);
+
+    // set domains/ranges
+    xScale.range([0,svg.width])
+        .domain(xDomain);
+    yScale.range([0,svg.height]);
+
+    let yAxis = d3.svg.axis()
+            .scale(yScale);
+
+    svg.append('g')
+        .attr('class','y-axis')
+        .call(yAxis);
+}
+
+let buildSvg = function(xScale, yScale){
+    
+}
